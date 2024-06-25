4945e2a94b5019acc41e157d36cd0bbd11a97a40
ashcrysis <arincommissions@gmail.com>
2024-06-25 14:07:40 -0300

    loading skeleton on pokemonList

--- src/components/PokemonList/index.tsx
+++ src/components/PokemonList/index.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import { Pokemon } from "../../pages/Search.tsx";
 import PokemonCard from "../PokemonCard/index.tsx";
-
+import { Skeleton } from "antd";
 import * as S from "./styles.ts";
 
 interface IPokemonListProps {
@@ -12,17 +11,30 @@ interface IPokemonListProps {
 
 const PokemonList: React.FC<IPokemonListProps> = (props) => {
   const { pokemonList, onClickCard } = props;
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <S.Container>
-      {pokemonList.map((pokemon, index) => {
-        return (
+      {loading ? (
+        <>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <Skeleton key={index} active />
+          ))}
+        </>
+      ) : (
+        pokemonList.map((pokemon, index) => (
           <PokemonCard
             key={`${pokemon.name}-${index}`}
             pokemon={pokemon}
             handleClick={onClickCard}
           />
-        );
-      })}
+        ))
+      )}
     </S.Container>
   );
 };
