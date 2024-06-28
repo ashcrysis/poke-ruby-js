60c2d4d7b7909ac590d3e5bd4a592af3c1cec46c
ashcrysis <arincommissions@gmail.com>
2024-06-28 10:03:04 -0300

    Update styles.ts

--- src/components/PokemonList/styles.ts
+++ src/components/PokemonList/styles.ts
@@ -10,3 +10,17 @@ export const Container = styled.div`
   height: 8%;
   margin-top: 140px;
 `;
+export const noDataContainer = styled.div`
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #444;
+  border-radius: 5px;
+  text-align: center;
+  background-color: #333;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+  color: white;
+
+  .ant-empty-description {
+    color: white;
+  }
+`;
